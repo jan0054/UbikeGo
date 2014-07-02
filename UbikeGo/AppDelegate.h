2d3bd7ff88a6074c2cc8ea5df97fcab873941b1c
Chi-sheng Jan <csjan@tapgo.cc>
2014-07-02 19:47:23 +0800

    Initial Commit

---
+++ UbikeGo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UbikeGo
+//
+//  Created by csjan on 7/2/14.
+//  Copyright (c) 2014 tapgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UbikeGo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UbikeGo
+//
+//  Created by csjan on 7/2/14.
+//  Copyright (c) 2014 tapgo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
